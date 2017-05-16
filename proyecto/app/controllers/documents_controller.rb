8b88ab6d9115ed472cc8d3585ece6345b0088582
jdvalenzuela <jdvalenzuela@miuandes.cl>
2017-05-16 18:48:07 -0400

    redcarpet complete

--- proyecto/app/controllers/documents_controller.rb
+++ proyecto/app/controllers/documents_controller.rb
@@ -64,7 +64,7 @@ class DocumentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_document
-      @document.text = markdown.render(@document.text)
+      @document = Document.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
