273ee3b52482a1bb7fca9e161d9ae7ec4aa9166a
Diego Beckdorf <dbeckdorf@miuades.cl>
2017-05-31 02:30:21 -0400

    Category uniqueness for Document scope validation

--- proyecto/app/models/categories_document.rb
+++ proyecto/app/models/categories_document.rb
@@ -2,10 +2,5 @@ class CategoriesDocument < ApplicationRecord
   belongs_to :category
   belongs_to :document
 
-  #validate :document_unique_categories?
-
-  #def document_unique_categories?
-  #  document_of_categories = CategoriesDocument.where(document: document, category: category)
-  #  errors.add :end_date, document_of_categories
-  #end
+  validates_uniqueness_of :category, :scope => [:document]
 end
