8f017544a5776e0fca4e031287da94d288938b66
jdvalenzuela <jdvalenzuela@miuandes.cl>
2017-06-21 21:01:33 -0400

    sign up listo

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
+  get 'users/sign_up' => redirect('users/sign_in')
+  get 'users/new' => redirect('users/sign_in')
+
   root to: 'documents#index'
   devise_for :users
   resources :users
